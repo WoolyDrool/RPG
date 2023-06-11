601f27b28cd96a756e1a9576e433f7a0946d0413
WoolyDrool <nikolai.mcneely15@gmail.com>
2023-06-10 20:53:55 -0700

    Added basic rotation

--- player/Player.gd
+++ player/Player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+@export var SPEED : float = 5.0
 const JUMP_VELOCITY = 4.5
+var turn_speed = 5
+var lookvector = Vector3.ZERO
+@export var model : CollisionShape3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -12,14 +15,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle Jump
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,5 +29,12 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	_process_turning()
 
 	move_and_slide()
+
+func _process_turning():
+	if velocity.length() > 0:
+		var look_direction = Vector2(velocity.z, velocity.x)
+		model.rotation.y = look_direction.angle()
